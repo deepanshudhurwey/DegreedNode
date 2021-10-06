5cf1ec195f877fdc41ca37e46b4ca36719648704
DeepanshuDhurwey <deepanshudhurwey2012@gmail.com>
2021-10-06 14:03:40 +0530

    made src folder structure

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.listen(4000,()=>{console.log('this App is running on 2000 port')})
+app.listen(4000,()=>{console.log('this App is running on 4000 port')})
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.send('hello user, welcome to the application')
 })
 
-app.get('/no',(req, res)=>{
-throw new Error('BRoken bridge')
+app.get('*',(req, res)=>{
+//throw new Error('BRoken bridge')
+res.render('404', {
+    title: '404',
+    name: 'Andrew Mead',
+    errorMessage: 'Page not found.'
+    })
 })
+
